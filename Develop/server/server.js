992a74d76f4de7947a2ca791be74b3396c8a4964
Nickhyman465 <nickhyman465@gmail.com>
2021-11-03 18:40:05 -0400

    Finished server side refactor; Beginning client side refractor

--- Develop/server/server.js
+++ Develop/server/server.js
@@ -2,11 +2,12 @@ const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
+const { authMiddleware } = require('./utils/auth')
 
 // require Apollo
 const { ApolloServer } = require('apollo-server-express');
 // require typeDefs and resolvers 
-const { typeDefs, resolvers } = require('./schema');
+const { typeDefs, resolvers } = require('./schemas');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -14,8 +15,10 @@ const PORT = process.env.PORT || 3001;
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  context: authMiddleware,
 })
 server.applyMiddleware({app});
+
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
