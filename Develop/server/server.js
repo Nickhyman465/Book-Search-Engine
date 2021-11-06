877e432573f9af7a8ee3ec0b6fef5d1a48e5d180
Nickhyman465 <nickhyman465@gmail.com>
2021-11-06 16:08:36 -0400

    fixed graphql playground issue

--- Develop/server/server.js
+++ Develop/server/server.js
@@ -18,12 +18,7 @@ const server = new ApolloServer({
   resolvers,
   context: authMiddleware,
 })
-const startApolloServer = async () => {
-  await server.start();
-  return server.applyMiddleware({ app });
-};
-
-startApolloServer();
+server.applyMiddleware({ app });
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -33,12 +28,21 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 app.use(routes);
 
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-  console.log(`API server running on port ${PORT}!`);
-  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`)
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+    // log where we can go to test our GQL API
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+  });
 });
+
+
+
+
+
